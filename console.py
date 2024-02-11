d76db941e03fbfe98f05c803a070d176a09b200c
Mafuyeka <tshepisoroshina@gmail.com>
2024-02-11 12:01:01 -0800

    Updated

---
+++ console.py
@@ -0,0 +1,27 @@
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program
+        Usage: quit
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """
+        Exit the program
+        """
+        print()  # Print a newline
+        return True
+
+    def emptyline(self):
+        """
+        Do nothing on empty line
+        """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
