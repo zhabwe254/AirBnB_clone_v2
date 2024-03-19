cb52af85dcbb6777fadd3c0f0b8b50cd964de31a
zhabwe254 <zhabwe@gmail.com>
2024-03-19 21:26:51 +0300

    a

--- console.py
+++ console.py
@@ -178,4 +178,9 @@ class HBNBCommand(cmd.Cmd):
 
     def do_update(self, arg):
         """Usage: update <class> <id> <attribute_name> <attribute_value> or
-       <class>.update(<id>, <attribute_name
+       <class>.update(<id>, <attribute_name>, <attribute_value>) or
+       <class>.update(<id>, <dictionary>)
+        Update a class instance of a given id by adding or updating
+        a given attribute key/value pair or dictionary."""
+        argl = parse(arg)
+        obj
