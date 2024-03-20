8a3d80feae8732beba3897ed09932636655320cd
Mafuyeka <tshepisoroshina@gmail.com>
2024-03-20 10:12:43 -0700

    Updated

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,71 +1,106 @@
-""" Module for testing BaseModel class """
-import unittest
+#!/usr/bin/python3
+""" """
 from models.base_model import BaseModel
-from datetime import datetime
-import pep8
+import unittest
+import datetime
+from uuid import UUID
+import json
+import os
 
 
-class TestBaseModel(unittest.TestCase):
-    """ Tests for the BaseModel class """
+class test_basemodel(unittest.TestCase):
+    """ """
 
-    @classmethod
-    def setUpClass(cls):
-        """ Set up for tests """
-        cls.base = BaseModel()
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = 'BaseModel'
+        self.value = BaseModel
 
-    @classmethod
-    def tearDownClass(cls):
-        """ Remove test instances """
-        del cls.base
+    def setUp(self):
+        """ """
+        pass
 
-    def test_pep8_conformance(self):
-        """ Test that the code conforms to PEP8 """
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/base_model.py'])
-        self.assertEqual(result.total_errors, 0, "PEP8 style errors found")
+    def tearDown(self):
+        try:
+            os.remove('file.json')
+        except:
+            pass
 
-    def test_documentation(self):
-        """ Test to see if documentation is created """
-        self.assertIsNotNone(BaseModel.__doc__)
-        self.assertIsNotNone(BaseModel.__init__.__doc__)
-        self.assertIsNotNone(BaseModel.__str__.__doc__)
-        self.assertIsNotNone(BaseModel.save.__doc__)
-        self.assertIsNotNone(BaseModel.to_dict.__doc__)
+    def test_default(self):
+        """ """
+        i = self.value()
+        self.assertEqual(type(i), self.value)
 
-    def test_attributes(self):
-        """ Test attributes """
-        self.assertTrue(hasattr(self.base, 'id'))
-        self.assertTrue(hasattr(self.base, 'created_at'))
-        self.assertTrue(hasattr(self.base, 'updated_at'))
+    def test_kwargs(self):
+        """ """
+        i = self.value()
+        copy = i.to_dict()
+        new = BaseModel(**copy)
+        self.assertFalse(new is i)
 
-    def test_id(self):
-        """ Test id """
-        base1 = BaseModel()
-        base2 = BaseModel()
-        self.assertNotEqual(base1.id, base2.id)
-
-    def test_created_updated(self):
-        """ Test created_at and updated_at """
-        self.assertTrue(hasattr(self.base, 'created_at'))
-        self.assertTrue(hasattr(self.base, 'updated_at'))
-        self.assertIsInstance(self.base.created_at, datetime)
-        self.assertIsInstance(self.base.updated_at, datetime)
-
-    def test_str(self):
-        """ Test __str__ method """
-        string = "[BaseModel] ({}) {}".format(self.base.id, self.base.__dict__)
-        self.assertEqual(str(self.base), string)
+    def test_kwargs_int(self):
+        """ """
+        i = self.value()
+        copy = i.to_dict()
+        copy.update({1: 2})
+        with self.assertRaises(TypeError):
+            new = BaseModel(**copy)
 
     def test_save(self):
-        """ Test save method """
-        old_updated_at = self.base.updated_at
-        self.base.save()
-        self.assertNotEqual(old_updated_at, self.base.updated_at)
+        """ Testing save """
+        i = self.value()
+        i.save()
+        key = self.name + "." + i.id
+        with open('file.json', 'r') as f:
+            j = json.load(f)
+            self.assertEqual(j[key], i.to_dict())
 
-    def test_to_dict(self):
-        """ Test to_dict method """
-        base_dict = self.base.to_dict()
-        self.assertEqual(self.base.__class__.__name__, 'BaseModel')
-        self.assertIsInstance(base_dict['created_at'], str)
-        self.assertIsInstance(base_dict['updated_at'], str)
-        self.assertIsInstance(base_dict['id'], str)
+    def test_str(self):
+        """ """
+        i = self.value()
+        self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
+                         i.__dict__))
+
+    def test_todict(self):
+        """ """
+        i = self.value()
+        n = i.to_dict()
+        self.assertEqual(i.to_dict(), n)
+
+    def test_kwargs_none(self):
+        """ """
+        n = {None: None}
+        with self.assertRaises(TypeError):
+            new = self.value(**n)
+
+    def test_kwargs_one(self):
+        """ """
+        n = {'Name': 'test'}
+        with self.assertRaises(KeyError):
+            new = self.value(**n)
+
+    def test_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.id), str)
+
+    def test_created_at(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.created_at), datetime.datetime)
+
+    def test_updated_at(self):
+        """ """
+        new = self.value()
+        #print(f"before save: {new.updated_at}")
+        self.assertEqual(type(new.updated_at), datetime.datetime)
+
+        import time
+        time.sleep(1)
+        new.save()
+
+        #print(f"after save: {new.updated_at}")
+        n = new.to_dict()
+        new = BaseModel(**n)
+        self.assertFalse(new.created_at == new.updated_at)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,48 +1,34 @@
+#!/usr/bin/python3
 """ Module for testing User class """
-import unittest
+from tests.test_models.test_base_model import test_basemodel
 from models.user import User
-from models.base_model import BaseModel
-import pep8
 
 
-class TestUser(unittest.TestCase):
-    """ Tests for the User class """
+class test_User(test_basemodel):
+ """ """
 
-    @classmethod
-    def setUpClass(cls):
-        """ Set up for tests """
-        cls.user = User()
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "User"
+        self.value = User
 
-    @classmethod
-    def tearDownClass(cls):
-        """ Remove test instances """
-        del cls.user
+    def test_first_name(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.first_name), str)
 
-    def test_pep8_conformance(self):
-        """ Test that the code conforms to PEP8 """
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/user.py'])
-        self.assertEqual(result.total_errors, 0, "PEP8 style errors found")
+    def test_last_name(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.last_name), str)
 
-    def test_documentation(self):
-        """ Test to see if documentation is created """
-        self.assertIsNotNone(User.__doc__)
+    def test_email(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.email), str)
 
-    def test_attributes(self):
-        """ Test attributes """
-        self.assertTrue(hasattr(self.user, 'email'))
-        self.assertTrue(hasattr(self.user, 'password'))
-        self.assertTrue(hasattr(self.user, 'first_name'))
-        self.assertTrue(hasattr(self.user, 'last_name'))
-
-    def test_inheritance(self):
-        """ Test inheritance """
-        self.assertIsInstance(self.user, BaseModel)
-
-    def test_types(self):
-        """ Test types """
-        self.assertIsInstance(self.user.email, str)
-        self.assertIsInstance(self.user.password, str)
-        self.assertIsInstance(self.user.first_name, str)
-       
-
+    def test_password(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.password), str)
