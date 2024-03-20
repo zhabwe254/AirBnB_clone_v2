537ee0e62735d4aa7b066f5473be1a4a54b27f1c
Mafuyeka <tshepisoroshina@gmail.com>
2024-03-20 10:01:44 -0700

    Updated

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-
-#BaseModel is a superclass that the Amenity class will inherit from models.base_model
 from models.base_model import BaseModel
 
-#Amenity class is used to represent different amenities available at properties, such as pool, building and parking
+
 class Amenity(BaseModel):
+      """Represent an amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
     name = ""
