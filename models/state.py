4b4e94bc6446dad7559491f6d66e681e2b57fc87
Mafuyeka <tshepisoroshina@gmail.com>
2024-03-20 06:14:59 -0700

    Updated

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """State Module for HBNB project"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
--- models/user.py
+++ models/user.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
-class User(BaseModel):
-
-     """Represent a User"""
+class User(BaseModel, Base):
+"""Represent a User.
 
      Attributes:
          email (str) : The email of the user.
     password (str) : The password of the user.
     first_name (str) : The first name of the user.
     last_name (str) : The last name of the user.
+"""
 
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
