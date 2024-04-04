103fb525dfaea6421db3a4ce7c5a17198b62dc5f
zhabwe254 <zhabwe@gmail.com>
2024-04-05 01:01:31 +0300

    a

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,26 +17,19 @@ def do_deploy(archive_path):
         folder_name = filename.split('.')[0]
         path = "/data/web_static/releases/{}/".format(folder_name)
 
-        # Upload archive
         put(archive_path, '/tmp/')
         run("mkdir -p {}".format(path))
 
-        # Uncompress archive
         run("tar -xzf /tmp/{} -C {}".format(filename, path))
 
-        # Delete archive
         run("rm /tmp/{}".format(filename))
 
-        # Move files out of inner folder
         run("mv {}web_static/* {}".format(path, path))
 
-        # Remove empty folder
         run("rm -rf {}web_static".format(path))
 
-        # Delete symbolic link
         run("rm -rf /data/web_static/current")
 
-        # Create new symbolic link
         run("ln -s {} /data/web_static/current".format(path))
 
         return True
