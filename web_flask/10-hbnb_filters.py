586ded11aee75e18f53bab647ef6a02ff7e2b71d
zhabwe254 <zhabwe@gmail.com>
2024-04-20 01:05:59 +0300

    a

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
 """
-Starts a Flask web application.
+Script that starts a Flask web application
 """
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
 
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters():
-    """Displays a HTML page like 6-index.html with HBNB filters."""
-    states = storage.all(State)
-    cities = storage.all(City)
-    amenities = storage.all(Amenity)
-    return render_template(
-        '10-hbnb_filters.html',
-        states=sorted(states.values(), key=lambda x: x.name),
-        cities=sorted(cities.values(), key=lambda x: x.name),
-        amenities=sorted(amenities.values(), key=lambda x: x.name)
-    )
-
-
 @app.teardown_appcontext
-def teardown(exception):
-    """Removes the current SQLAlchemy Session."""
+def teardown_db(exception):
+    """
+    Closes the storage on teardown
+    """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """
+    Renders a HTML page with filters for states, cities, and amenities
+    """
+    states = storage.all("State").values()
+    amenities = storage.all("Amenity").values()
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
